d6483c7891b5ec110c4cff17b77a0bffcd6f262e
Wade Croft <wadecroft@gmail.com>
2021-06-08 14:42:46 -0700

    added DayList and modified daylistitem

---
+++ src/components/DayList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import DayListItem from "components/DayListItem";
+
+export default function DayList(props) {
+
+  const list = props.days.map((day, index)=>{
+   return (<DayListItem 
+  name={day.name}
+  key={index} 
+  spots={day.spots} 
+  selected={day.name === props.day}
+  setDay={props.setDay}  />);
+  });
+
+   return (
+     <ul>{list}</ul>
+   );
+ }
--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -8,10 +8,13 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": (props.spots === 0)
   });
+
+  const formatSpots = `${!props.spots ? 'no' : props.spots} spot${props.spots !== 1 ? 's' : ''} remaining`;
+
   return (
     <li className={dayClass}onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots}</h3>
     </li>
   );
 }
