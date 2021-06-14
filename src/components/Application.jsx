bf0058c9d3ce2e1525082a967346c6da3e271081
Wade Croft <wadecroft@gmail.com>
2021-06-14 12:37:57 -0700

    Verified spots remaining is working properly

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -32,7 +32,7 @@ export default function Application(props) {
         setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }))
       })
       .catch((error)=> {console.error(error)})
-    }, []);
+    });
 
     const appointments = getAppointmentsForDay(state, state.day);
 
