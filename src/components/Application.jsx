f52a1b23af00b5fa8f8cd90c0c14f276cb0e2048
Wade Croft <wadecroft@gmail.com>
2021-06-16 20:31:14 -0700

    fixed a few more bugs

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -6,7 +6,6 @@ import DayList from "components/DayList";
 import "components/Appointment";
 import Appointment from "components/Appointment";
 import { getAppointmentsForDay, getInterview } from "helpers/selectors";
-import { getInterviewersForDay } from "helpers/selectors";
 import useApplicationData from "hooks/useApplicationData";
 
 
@@ -30,7 +29,8 @@ export default function Application(props) {
           setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }))
         })
         .catch((error)=> {console.error(error)})
-      }, [interviewBooked]);
+      }, [interviewBooked, setState]);
+      console.log('pooop');
 
     const appointments = getAppointmentsForDay(state, state.day);
 
